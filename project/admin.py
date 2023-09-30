afb39cd13f4181d78afd57dec4b728d541109d07
Nidal Chateur <nidalchateur@gmail.com>
2023-09-30 10:15:31 +0200

    project.admin update

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from project.models import Project, Contributor, Issue, Comment
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "name")
+
+
+admin.site.register(Project, ProjectAdmin)
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("id", "contributor", "project")
+
+
+admin.site.register(Contributor, ContributorAdmin)
+
+
+class IssuerAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "name", "project")
+
+
+admin.site.register(Issue, IssuerAdmin)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "author", "issue")
+
+
+admin.site.register(Comment, CommentAdmin)
